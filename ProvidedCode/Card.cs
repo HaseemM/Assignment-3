32b5219ba967a9667b189ebb9398d00c92217bb7
HaseemM <157539290+HaseemM@users.noreply.github.com>
2024-04-19 20:54:32 -0400

    Add files via upload

---
+++ ProvidedCode/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.ProvidedCode
+{
+    // DO NOT MODIFY THIS FILE
+
+    public abstract partial class Card
+    {
+        public CardColor Color { get; set; }
+
+        // method for checking if this card can be placed on the previously played card on the discard pile.
+        // returns true if card can be played.
+        public abstract bool IsPlayable(Card previousCard);
+    }
+
+    // Possible card colors
+    public enum CardColor
+    {
+        Red, Green, Yellow, Blue
+    }
+}
---
+++ ProvidedCode/Interfaces.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.ProvidedCode
+{
+    // DO NOT MODIFY THIS FILE
+
+    // interface for cards that have a color
+    interface IColorCard
+    {
+        // Color used for specify the color of the card.
+        // For wild cards, Color used for storing the user's choice on what color card comes next.
+        CardColor Color { get; set; }
+    }
+
+
+    // interface for action cards
+    interface IActionCard
+    {
+        // Specifies the action type of the card.
+        CardAction Action { get; }
+    }
+
+
+    // Types of actions used by action cards (both color action cards and wild action cards)
+    public enum CardAction
+    {
+        // color action cards
+        Reverse, Skip, DrawTwo,
+
+        // wild action cards
+        Wild, WildDrawFour, WildSwapHands, WildShuffleHands
+    }
+}
